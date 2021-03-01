0ee9067290514fa28b794528c541f5b46ea14196
randomuser <luoxiao1106@gmail.com>
2021-03-01 10:53:10 -0800

    fixed db connect issue

--- databaseConnection.js
+++ databaseConnection.js
@@ -13,7 +13,7 @@ const dbConfigHeroku = {
 const dbConfigLocal = {
   host: "localhost",
   user: "root",
-  password: "Password",
+  password: "xraptorl",
   database: "lab_example",
   multipleStatements: false,
 };
